9fa0492eda628265203b4418e906b7741e14af53
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2024-07-02 21:50:21 +0200

    add home, info pages

--- RiffMates/RiffMates/RiffMates/urls.py
+++ RiffMates/RiffMates/RiffMates/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from home import views as home_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("",home_views.home),
+    path("credits/",home_views.credits),
+    path("about/",home_views.about),
+    path("info/",home_views.info),
+    path("info-json/",home_views.info_json),
+
+    
+    
 ]
---
+++ RiffMates/RiffMates/home/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render,HttpResponse
+from django.http import JsonResponse
+
+# Create your views here.
+
+def home(request):
+    return HttpResponse("<h2>Home</h2>")
+
+def credits(request):
+    content = "Ricky\nYour name  "
+    return HttpResponse(content, content_type='text/plain')
+
+def about(request):
+    content = """
+    <h2>About Me</h2>
+    <p>
+    I am Uyghur and I am a python developer.
+    </p>
+    """
+    return HttpResponse(content)
+
+def info_json(request):
+     content = {
+        "version":"1.0",
+        "name":"Music app",
+        "author":"Uyghur",
+        "date":"2024-09-23"
+    }
+     return JsonResponse(content)
+    
+
+def info(request):
+    content = {
+        "version":"1.0",
+        "name":"Music app",
+        "author":"Uyghur",
+        "date":"2024-09-23"
+    }
+    
+    information = [f"<h3>{k} : {v}<h3>" for k,v in content.items()]
+    
+    return HttpResponse(information)
