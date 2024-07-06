4bc7fd335fa7229cb0cbc50ae01fc45e98753520
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2024-07-06 21:56:46 +0200

    get books from db and display it

--- Book_Store/Book_Store/urls.py
+++ Book_Store/Book_Store/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from book_outlet import views as book_outlet_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('books/',book_outlet_views.insert_data),
+    path('',  include('book_outlet.urls')),
 ]
--- Book_Store/book_outlet/views.py
+++ Book_Store/book_outlet/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse,Http404,get_object_or_404
+from django.db import models
+from book_outlet.models import Book
 
 # Create your views here.
+
+data =[
+    [1,'Lord of the Rings',  3, "KJR", True],
+    [2,'Uyghuristan history', 5, "Atilla", True],
+    [3,'Java programming', 5, "Camilla", True],
+    ]
+
+def index(request):
+    books  = Book.objects.all()
+    
+    return render(request,"book_outlet/index.html", {"books":books})
+
+def book_detail(request,id):
+    # try:
+    #     book = Book.objects.get(id=id)
+    # except:
+    #     raise Http404()
+    book = get_object_or_404(Book,id=id)
+    return render(request,"book_outlet/book_detail.html", {
+        "title": book.title,
+        "author": book.author,
+        "rating": book.rating,
+        "is_bestselling": book.is_bestselling,
+     })
+    
+
+
+def insert_data(request):
+    for value in data:
+        book = Book(value[0],value[1],value[2],value[3],value[4])
+        book.save()
+        # Book.objects.create(value[0],value[1],value[2],value[3],value[4])
+    return HttpResponse('<h1>Book saved</h1>')
