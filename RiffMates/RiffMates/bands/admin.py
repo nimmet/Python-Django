19dc7ceedb5396ca7419b08bd8bb22ac6b9da212
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2024-07-07 17:03:12 +0200

    add pagination

--- RiffMates/RiffMates/bands/admin.py
+++ RiffMates/RiffMates/bands/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from bands.models import Musician
 
 # Register your models here.
+
+admin.site.register(Musician)
--- RiffMates/RiffMates/bands/views.py
+++ RiffMates/RiffMates/bands/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,get_object_or_404
 from bands.models import Musician
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -12,5 +13,23 @@ def musician(request, musician_id):
     return render(request,"musician.html",data)
 
 def musicians(request):
+    all_musicians = Musician.objects.all().order_by('first_name')
+    paginator = Paginator(all_musicians,2)
     
-    return render(request,"musicians.html", {"musicians":Musician.objects.all().order_by('first_name')})
+    page_num = int(request.GET.get('page',1))
+    
+    if page_num < 1:
+        page_num = 1
+    elif page_num > paginator.num_pages:
+        page_num = paginator.num_pages
+    
+    page = paginator.page(page_num)
+    
+    data = {
+        "musicians": page.object_list,
+        "page": page,
+    }
+        
+    
+    
+    return render(request,"musicians.html",data)
