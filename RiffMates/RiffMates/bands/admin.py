f271cb8375afefcea993981ffabc24b9b7faada7
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2024-07-09 21:18:59 +0200

    create ModelAddmin class for getting more flexible admin

--- RiffMates/RiffMates/bands/admin.py
+++ RiffMates/RiffMates/bands/admin.py
@@ -4,8 +4,18 @@ from bands.models import Musician, Room, Venue, Band
 
 
 # Register your models here.
+class MusicianAdmin(admin.ModelAdmin):
+    list_display = ("id","last_name","show_weekday")
+    search_fields = ("first_name","last_name",)
+    
+    
+    def show_weekday(self,obj):
+        return obj.birth.strftime("%A")
+    
+    show_weekday.short_description ="Birth Weekday"
+    
 
-admin.site.register(Musician)
+admin.site.register(Musician, MusicianAdmin)
 admin.site.register(Venue)
 admin.site.register(Room)
 admin.site.register(Band)
