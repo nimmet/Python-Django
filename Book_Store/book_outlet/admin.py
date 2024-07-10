4a877515106330de24720cf7dd8729c69b37f656
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2024-07-10 10:59:31 +0200

    db relations

--- Book_Store/book_outlet/admin.py
+++ Book_Store/book_outlet/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Author,Address, Country
 
 # Register your models here.
 
@@ -9,6 +9,22 @@ class BookAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
     list_filter = ("author","rating",)
     list_display = ("author","title")
+    
+
+class AutherAdmin(admin.ModelAdmin):
+  
+    list_display = ("first_name","last_name","id","show_city")
+    list_filter = ("first_name","last_name")
+    
+    def show_city(self,obj):
+        return obj.address.city    
+    
+    show_city.short_description ="City"
+
+    
 
 
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AutherAdmin)
+admin.site.register(Address)
+admin.site.register(Country)
