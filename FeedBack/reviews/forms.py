4ad600730b24dadb960cc6e1e5d22aa89872030b
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2024-07-10 21:40:31 +0200

    Django form,  field and save form data  to the db

--- FeedBack/reviews/forms.py
+++ FeedBack/reviews/forms.py
@@ -7,3 +7,8 @@ class ReviewForrm(forms.Form):
         "required": "You name must not be empty!",
         "max_length": "Please enter a shorter name!",
     })
+    
+    review_text = forms.CharField(label="Your Feedback", widget=forms.Textarea,
+                                  max_length=200)
+    rating = forms.IntegerField(label="Your Rating",min_value=1, max_value=5)
+    
--- FeedBack/reviews/views.py
+++ FeedBack/reviews/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import ReviewForrm
+from .models import Review
 
 
 # Create your views here.
@@ -12,6 +13,10 @@ def review(request):
         form = ReviewForrm(request.POST)
         
         if form.is_valid():
+            review = Review(user_name=form.cleaned_data['user_name'],
+                            review_text=form.cleaned_data['review_text'],
+                            rating=form.cleaned_data['rating'])
+            review.save()
             return HttpResponseRedirect("/thank-you")
         
     else:
