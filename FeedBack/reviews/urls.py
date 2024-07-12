747a8730d45bfafefb67fd466cdab771bc3429b7
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2024-07-12 16:50:10 +0200

    session

--- FeedBack/reviews/urls.py
+++ FeedBack/reviews/urls.py
@@ -4,5 +4,6 @@ urlpatterns = [
     path("",views.ReviewView.as_view()),
     path("thank-you",views.ThankyouView.as_view()),
     path("reviews",views.ReviewsListView.as_view()),
+    path("reviews/favorite", views.AddFavoriteView.as_view()),
     path("reviews/<int:pk>",views.SingleReviewView.as_view())
 ]
