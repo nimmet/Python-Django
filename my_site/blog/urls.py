5f8855b079aa832288c8e53e23c321d3f580391a
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2024-07-13 16:20:17 +0200

    add 'read later' features and remove it

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("", views.StartingPageView.as_view(), name="starting_page"),
     path("posts", views.AllPostsView.as_view(), name="posts_page"),
     path("posts/<slug:slug>",views.SinglePostView.as_view(), name="post_detail_page"),
+    path("read-later", views.ReadLaterView.as_view(), name="read-later")
     
 ]
  
