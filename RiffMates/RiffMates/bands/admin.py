1cccff44bc818064c99f9010e4578aa45422eff7
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2024-07-17 21:34:29 +0200

    queryset, list_filter,facets

--- RiffMates/RiffMates/bands/admin.py
+++ RiffMates/RiffMates/bands/admin.py
@@ -1,18 +1,55 @@
 from django.contrib import admin
 from bands.models import Musician, Room, Venue, Band
+from datetime import datetime, date
 
 
 
 # Register your models here.
+
+    
+
+class DecadeListFilter(admin.SimpleListFilter):
+    title = "decade born"
+    parameter_name = "decade"
+    
+    def lookups(self, request, model_admin):
+        result = []
+        this_year = datetime.today().year
+        this_decade = (this_year // 10) * 10
+        start = this_decade- 10
+        for year in range(start, start- 100,-10):
+            result.append( (str(year), f"{year}-{year+9}") )
+        return result
+    
+    def queryset(self, request, queryset):
+        start = self.value()
+        if start is None:
+            return queryset
+        
+        start = int(start)
+        result = queryset.filter(
+        birth__gte=date(start, 1, 1),
+        birth__lte=date(start + 9, 12, 31),
+        )
+        return result
+    
+
 class MusicianAdmin(admin.ModelAdmin):
-    list_display = ("id","last_name","show_weekday")
+    # list_display = ("id","last_name","show_weekday")
     search_fields = ("first_name","last_name",)
+    list_display = ("id", "last_name", "first_name", "birth","show_weekday", )
+    list_filter = (DecadeListFilter, )
     
     
     def show_weekday(self,obj):
         return obj.birth.strftime("%A")
     
     show_weekday.short_description ="Birth Weekday"
+    
+    
+    
+    
+    
     
 
 admin.site.register(Musician, MusicianAdmin)
